1098b0f4eb31991c638cd40b8b756a23a0232c28
Kiran Anilkumar <kiran.anil97@gmail.com>
2021-02-06 15:36:27 +0530

    Python Task 1

--- area_of_circle.py
+++ area_of_circle.py
@@ -1,5 +1,3 @@
-f = input('Input the Filename: ')
-if f.endswith('.py'):
-    print('The extension of the file is: Python');
-else :
-    print('Not a python file!')
+r = input('Input the radius of the circle: ')
+area = 3.1415926535897932384 * float(r) * float(r)
+print('The area of the circle with radius',float(r),'is:',float(area))
