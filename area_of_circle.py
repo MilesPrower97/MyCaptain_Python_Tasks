f8f9a8f306357bcf956f794e41c56712bbeb5246
Kiran Anilkumar <kiran.anil97@gmail.com>
2021-02-06 15:26:09 +0530

    Python Task 1

---
+++ area_of_circle.py
@@ -0,0 +1,5 @@
+f = input('Input the Filename: ')
+if f.endswith('.py'):
+    print('The extension of the file is: Python');
+else :
+    print('Not a python file!')
---
+++ extension.py
@@ -0,0 +1,17 @@
+f = input('Input the Filename: ')
+if f.endswith('.py'):
+    print('The extension of the file is: Python')
+elif f.endswith('.txt'):
+    print('The extension of the file is: Text')
+elif f.endswith('.html'):
+    print('The extension of the file is: Hypertext Markup Language')
+elif f.endswith('.php'):
+    print('The extension of the file is: PHP')
+elif f.endswith('.doc'):
+    print('The extension of the file is: Document')
+elif f.endswith('.ppt'):
+    print('The extension of the file is: Powerpoint Presentation')
+elif f.endswith('.css'):
+    print('The extension of the file is: Cascading Style Sheets')
+else :
+    print('Not Applicable Now!!!')
